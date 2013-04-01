716787bc302cdddeef9480ce681edf83d0fea788
Talos Tse <talos.tse@gmail.com>
2013-04-01 15:32:11 +0800

    导入库

---
+++ data.py
@@ -0,0 +1,25 @@
+'''
+Created on Apr 1, 2013
+
+@author: talos
+'''
+
+class k_data():
+    '''
+    classdocs
+    '''
+    
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+    
+    
+    def format(self,data):
+        pass
+
+
+if __name__ == '__main__':
+    pass
